78f41e3bbe05da5b27d1f677e43431e333c2a36d
Thaylon Guedes Santos <thaylongs@gmail.com>
2017-11-04 20:17:54 -0200

    Improving the performance of Database connection.

--- core/src/main/scala/br/uff/spark/database/CassandraDBDao.scala
+++ core/src/main/scala/br/uff/spark/database/CassandraDBDao.scala
@@ -12,25 +12,40 @@ class CassandraDBDao(val execution: Execution) extends DataBaseBasicMethods {
 
   val con = DataSource.getConnection
 
+  private val STMT_INSERT_TASK = con.prepare(
+    """
+      |INSERT INTO dfanalyzer."Task"
+      |            ("executionID", id, description, "transformationType")
+      |VALUES
+      |            (?, ?, ?, ?);
+    """.stripMargin)
+
+  private val STMT_DEPENDENCIES_OF_TASK = con.prepare(
+    """
+      |INSERT INTO dfanalyzer."DependenciesOfTask"
+      |            ("executionID", target, "source")
+      |VALUES
+      |            (?, ?, ?);
+    """.stripMargin)
+
   override def insertTask(task: Task): Unit = {
-    var sql = "INSERT INTO dfanalyzer.\"Task\" (\"executionID\", id, description, \"transformationType\") VALUES(?, ?, ?, ?);"
-    var stmp = con.prepare(sql)
-      .bind(
+
+    con.executeAsync(
+      STMT_INSERT_TASK.bind(
         execution.ID,
         task.id,
         task.description,
         task.transformationType.toString
       )
-    con.execute(stmp)
+    )
 
-    sql = "INSERT INTO dfanalyzer.\"DependenciesOfTask\" (\"executionID\", target, \"source\") VALUES(?, ?, ?);"
-    stmp = con.prepare(sql)
-      .bind(
+    con.executeAsync(
+      STMT_DEPENDENCIES_OF_TASK.bind(
         execution.ID,
         task.id,
         task.dependenciesIDS.toSet.asJava
       )
-    con.execute(stmp)
+    )
   }
 
 
@@ -38,101 +53,163 @@ class CassandraDBDao(val execution: Execution) extends DataBaseBasicMethods {
     DataSource.close()
   }
 
-  override def updateExecution(execution: Execution): Unit = {
-    val sql = "UPDATE dfanalyzer.\"Execution\" SET  \"EndTime\"=?  WHERE ID=?;"
+  val STMT_UPDATE_EXECUTION = con.prepare(
+    """
+      |UPDATE dfanalyzer."Execution"
+      |SET    "EndTime" = ?
+      |WHERE  id = ?;
+    """.stripMargin)
 
-    val stmp = con.prepare(sql)
-      .bind(
+  override def updateExecution(execution: Execution): Unit = {
+    con.executeAsync(
+      STMT_UPDATE_EXECUTION.bind(
         Timestamp.from(execution.endTime.toInstant(ZonedDateTime.now(ZoneId.systemDefault()).getOffset)),
         execution.ID
       )
-    con.execute(stmp)
+    )
 
   }
 
-  override def init(): Unit = {
+  val STMT_INSERT_EXECUTION = con.prepare(
+    """
+      | INSERT INTO dfanalyzer."Execution"
+      |            (id, "StartTime", "EndTime", "AplicationName")
+      |VALUES
+      |            (?, ?, ?, ?);
+    """.stripMargin)
 
-    val sqlInsertExecution = "INSERT INTO dfanalyzer.\"Execution\"(id, \"StartTime\", \"EndTime\", \"AplicationName\") VALUES (?, ?, ?, ?);"
-    val stmp = con.prepare(sqlInsertExecution)
-      .bind(
+  override def init(): Unit = {
+    con.executeAsync(
+      STMT_INSERT_EXECUTION.bind(
         execution.ID,
         Timestamp.from(execution.startTime.toInstant(ZonedDateTime.now(ZoneId.systemDefault()).getOffset)),
         null,
         execution.name
       )
-    con.execute(stmp)
-
+    )
   }
 
+  val STMT_INSERT_DATA_ELEMENT = con.prepare(
+    """
+      |INSERT INTO dfanalyzer."DataElement"
+      |            (id, value, "executionID")
+      |VALUES
+      |            (?, ?, ?);
+    """.stripMargin)
+
   override def insertDataElement(dataElement: DataElement[_ <: Any]): Unit = {
-    val sqlInsertDataElement = "INSERT INTO dfanalyzer.\"DataElement\" (id, value, \"executionID\") VALUES(? , ? , ?);"
-    val stmp = con.prepare(sqlInsertDataElement)
-      .bind(
+    con.executeAsync(
+      STMT_INSERT_DATA_ELEMENT.bind(
         dataElement.id,
         dataElement.value.toString,
         execution.ID
       )
-    con.execute(stmp)
+    )
 
     if (!dataElement.dependenciesIDS.isEmpty)
       insertDependencies(dataElement)
   }
 
+  val STMT_INSERT_DEPENDENCIES_OF_DATA_ELEMENT = con.prepare(
+    """
+      | INSERT INTO dfanalyzer."DependenciesOfDataElement"
+      |            (source, target, task, "executionID")
+      |VALUES
+      |            (?, ?, ?, ?);
+    """.stripMargin)
+
   override def insertDependencies(dataElement: DataElement[_ <: Any]): Unit = {
-    val sqlInsertDataElementDependencies = "INSERT INTO dfanalyzer.\"DependenciesOfDataElement\" (source, target, task, \"executionID\") VALUES(?, ?, ?,?);"
-    val stmp = con.prepare(sqlInsertDataElementDependencies)
-      .bind(
+    con.executeAsync(
+      STMT_INSERT_DEPENDENCIES_OF_DATA_ELEMENT.bind(
         dataElement.dependenciesIDS.toSet.asJava,
         dataElement.id,
         dataElement.task.id,
         execution.ID
       )
-    con.execute(stmp)
+    )
   }
 
+  val STMT_UPDATE_DEPENDENCIES_OF_DATA_ELEMENT = con.prepare(
+    """
+      |UPDATE dfanalyzer."DependenciesOfDataElement"
+      |SET    source = source + ?
+      |WHERE  target = ?
+      |       AND task = ?
+      |       AND "executionID" = ?
+    """.stripMargin)
+
   override def insertDependencieOfDataElement(dataElement: DataElement[_ <: Any], id: UUID): Unit = {
-    val sqlInsertDataElementDependencies = "UPDATE dfanalyzer.\"DependenciesOfDataElement\" SET source = source + ?  where target=?  and task=?  and \"executionID\" =?"
-    val stmp = con.prepare(sqlInsertDataElementDependencies)
-      .bind(
+    con.executeAsync(
+      STMT_UPDATE_DEPENDENCIES_OF_DATA_ELEMENT.bind(
         Set(id).asJava,
         dataElement.id,
         dataElement.task.id,
         execution.ID
       )
-    con.execute(stmp)
+    )
   }
 
+
+  val STMT_UPDATE_DATA_ELEMENT = con.prepare(
+    """
+      |UPDATE
+      |   dfanalyzer."DataElement"
+      |SET
+      |   value = ?
+      |WHERE
+      |     "executionID"=?
+      |  AND
+      |     id=?;
+    """.stripMargin)
+
   override def updateDataElement(dataElement: DataElement[_ <: Any]): Unit = {
-    val sql = "UPDATE dfanalyzer.\"DataElement\" SET value = ? WHERE \"executionID\"=? and id=?;"
-    val stmp = con.prepare(sql)
-      .bind(
+    con.executeAsync(
+      STMT_UPDATE_DATA_ELEMENT.bind(
         dataElement.value.toString,
         execution.ID,
         dataElement.id
       )
-    con.execute(stmp)
+    )
   }
 
+  val STMT_DELETE_DEPENDENCIES_OF_DATA_ELEMENT = con.prepare(
+    """
+      |DELETE
+      |FROM dfanalyzer."DependenciesOfDataElement"
+      |WHERE
+      |       "executionID"=?
+      |  AND
+      |       task=?
+      |  AND
+      |       target=?
+    """.stripMargin)
+
+  val STMT_DELETE_DATA_ELEMENT = con.prepare(
+    """
+      |DELETE
+      |FROM dfanalyzer."DataElement"
+      |WHERE
+      |       "executionID"=?
+      |  AND
+      |       id=?
+    """.stripMargin)
 
   override def deleteDataElement(dataElement: DataElement[_ <: Any]): Unit = {
-    val sqlDeleteDependencies = "DELETE FROM  dfanalyzer.\"DependenciesOfDataElement\" WHERE \"executionID\"=? and task=? and target=?"
-    var stmp = con.prepare(sqlDeleteDependencies)
-      .bind(
+    con.executeAsync(
+      STMT_DELETE_DEPENDENCIES_OF_DATA_ELEMENT.bind(
         execution.ID,
         dataElement.task.id,
         dataElement.id
       )
-    con.execute(stmp)
+    )
 
-    val sqlDeleteDataElement = "DELETE FROM dfanalyzer.\"DataElement\" WHERE \"executionID\"=?  and  id=?"
-    stmp = con.prepare(sqlDeleteDataElement)
-      .bind(
+    con.executeAsync(
+      STMT_DELETE_DATA_ELEMENT.bind(
         execution.ID,
         dataElement.id
       )
-    con.execute(stmp)
+    )
   }
-
 
   override def allFilesOfExecution(id: UUID, onRead: (String, String) => Unit): Unit = {
     val sqlDeleteDependencies = ""
