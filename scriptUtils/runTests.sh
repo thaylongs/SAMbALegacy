3556cfdbfad12a8b5758ac66ab3d83b390b566a7
Thaylon Guedes Santos <thaylongs@gmail.com>
2018-05-27 03:12:41 -0300

    SAMbA Docker Build

--- scriptUtils/runTests.sh
+++ scriptUtils/runTests.sh
@@ -3,4 +3,4 @@ export ENABLE_PROVENANCE=FALSE
 export ENABLE_VCS=FALSE
 
 export MAVEN_OPTS="-Xss1500m"
-./build/mvn  -Phadoop-2.7 -Dhadoop.version=2.7.3  compile package
+./build/mvn -Phadoop-2.7 -Dhadoop.version=2.7.3  -Pscala-2.11 compile package
